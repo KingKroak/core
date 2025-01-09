8aaba7d0d811fbd28ae6ebc16adc220f06ae6778
KingKroak <atraxa.investments@gmail.com>
2025-01-09 21:32:44 +1100

    updated

--- processes/emailer_test.py
+++ processes/emailer_test.py
@@ -4,7 +4,7 @@ from utils.gmail import GmailService
 
 if __name__ == '__main__':
     print(os.getcwd())
-    gmail_service = GmailService('../utils/token.json', '../utils/credentials.json')
+    gmail_service = GmailService(f'{os.getcwd()}/utils/token.json', f'{os.getcwd()}/utils/credentials.json')
     gmail_service.send_email(
         'AtraxaBot',
         'atraxa.investments@gmail.com',
